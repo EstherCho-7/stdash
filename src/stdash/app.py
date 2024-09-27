235ffa19ebbb0f946f14b57dcb20f862f0f574b1
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-27 10:25:49 +0900

    app.py

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -14,21 +14,6 @@ def load_data():
 data=load_data()
 df=pd.DataFrame(data)
 
-# TODO
-# request_time, prediction_time 이용하여 '%Y-%m-%d %H' 형식
-# 즉, 시간별 GROUP BY COUNT하여 matplotlib 차트 그려보기
-
-#df['request_time']=pd.to_datetime()
-#df['request_time'].dt.strftime('%Y-%m-%d %H')
-#df.groupby('').size()
-
-#plt.bar()
-#plt.plot()
-#plt.xlabel()
-
-#화면에 그리기
-#st.pyplot(plt)
-
 df['request_time']=pd.to_datetime(df['request_time'])
 df['re_formatted_time']=df['request_time'].dt.strftime('%Y-%m-%d %H')
 requestt=df.groupby('re_formatted_time').size()
@@ -42,6 +27,23 @@ plt.bar(requestt.index, requestt.values, width=0.4, label='request', color='blue
 plt.plot(predictiont.index, predictiont.values, marker='^', label='predicton', color='red')
 
 plt.title('Request and Prediction Counts over Time')
+plt.xlabel('time')
+plt.ylabel('count')
+
+st.pyplot(plt)
+
+df['request_user']=df['request_user'].astype(str)
+requestu=df.groupby('request_user').size()
+
+df['prediction_model']=df['prediction_model'].astype(str)
+predictionm=df.groupby('prediction_model').size()
+
+
+plt.figure(figsize=(16, 8))
+plt.bar(requestu.index, requestu.values, width=0.4, label='request', color='blue', align='center')
+plt.bar(predictionm.index, predictionm.values, width=0.4, label='predicton', color='red', align="edge")
+
+plt.title('Request user(BLUE) and Prediction(RED) Counts over Time')
 plt.xlabel('time')
 plt.ylabel('count')
 
